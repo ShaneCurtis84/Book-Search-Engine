d991aa4505081b0f42b29e690e65fec0d555f1e1
Shane Curtis <c84a@ymail.com>
2021-10-24 19:45:16 +0800

    Fixed errors. Can now signup/Login. Add and remove books

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,13 +2,17 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+
 import Auth from '../utils/auth';
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "../utils/mutations";
+
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, { error, }] = useMutation(LOGIN);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,15 +30,14 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
+      // Authenticating login with the token
+      Auth.login(data.login.token);
+      if (error) {
         throw new Error('something went wrong!');
       }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
